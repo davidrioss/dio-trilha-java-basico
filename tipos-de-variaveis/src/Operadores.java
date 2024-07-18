1783f28ac2a244dae732840e36525bb9992b53d9
David Rios <55660690+davidrioss@users.noreply.github.com>
2024-07-18 12:38:50 -0300

    novas classes

---
+++ tipos-de-variaveis/src/Operadores.java
@@ -0,0 +1,31 @@
+public class Operadores {
+    public static void main(String[] args) {
+        String concatenacao ="?"; 
+
+        concatenacao = 1+1+1+"1";
+        
+        concatenacao = 1+"1"+1+1;
+        
+        concatenacao = 1+"1"+1+"1";
+        
+        concatenacao = "1"+1+1+1;
+        
+        concatenacao = "1"+(1+1+1);  
+
+        int numero = 5;
+
+        numero = - numero;
+
+        numero = - numero;
+
+        System.out.println(numero);
+
+
+        String nomeUm = "David";
+
+        String nomeDois = new String("David");
+
+        System.out.println(nomeUm == nomeDois); //so serve em vaariaveis
+        System.out.println(nomeUm.equals(nomeDois)); // usado para comparar objetos
+    }
+}
---
+++ tipos-de-variaveis/src/TiposDeVariaveis.java
@@ -0,0 +1,13 @@
+public class TiposDeVariaveis {
+    public static void main(String[] args) throws Exception {
+     
+        final double VALOR_PI = 3.14;
+
+        System.out.println(VALOR_PI);
+
+        // float precisa ter o F no final do numero tipo float x = 12.354F
+        // long precisa ter o L no final do numero tipo long x = 12354L
+
+
+    }
+}
