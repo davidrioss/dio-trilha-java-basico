f57911518c9e0792644c9dff24515d3df447ea1b
David Rios <55660690+davidrioss@users.noreply.github.com>
2024-07-18 20:26:23 -0300

    Criação do projeto para exercicio Conta Bancaria

---
+++ ContaBanco/src/ContaTerminal.java
@@ -0,0 +1,11 @@
+public class ContaTerminal {
+    public static void main(String[] args) throws Exception {
+        
+        //TODO: conhecer e importar a classe Scanner
+        //exibir as mensagens para o nosso usuario
+        //Obter pela Scanner os valores digitados no terminal
+        //Exibir a mensagem conta criada
+
+
+    }
+}
